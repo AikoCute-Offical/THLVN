70f8e3f3607684fb382b4a0d32666be4ff453194
AikoCute <aikocute@icloud.com>
2022-11-21 15:33:05 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,20 +7,16 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-cur_dir=$(pwd)
-
-# check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Error：${plain} This script must be run as root user!\n" && exit 1
-
+[[ $EUID -ne 0 ]] && echo -e "${red}Error:${plain} This script must be run as root!" && exit 1
 # install English
 english(){
-    bash <(curl -Ls https://raw.githubusercontent.com/AikoCute-Offical/THLVN/main/AikoR-en.sh)
+  bash <(curl -Ls https://raw.githubusercontent.com/AikoCute-Offical/THLVN/main/AikoR-en.sh)
 }
 
 
 # install vietnamese
 vietnamese(){
-    bash <(curl -Ls https://raw.githubusercontent.com/AikoCute-Offical/THLVN/main/AikoR-vi.sh)
+   bash <(curl -Ls https://raw.githubusercontent.com/AikoCute-Offical/THLVN/main/AikoR-vi.sh)
 }   
 
 
